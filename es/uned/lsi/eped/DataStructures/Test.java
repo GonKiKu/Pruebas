ece4c822d0b0583f475f61562fd87eb7d1a4122e
javie <javie@192.168.1.129>
2021-02-26 21:38:23 +0100

    hola

--- es/uned/lsi/eped/DataStructures/Test.java
+++ es/uned/lsi/eped/DataStructures/Test.java
@@ -66,7 +66,7 @@ public class Test {
 			System.out.print(iteratorList.getNext()+" ");
 		}
 		
-		/*
+		/*hola
 		 //Test operaciones sobre pilas
 		System.out.println("Test de operaciones sobre pilas");
 		Stack<Integer> stack = new Stack<Integer>();
